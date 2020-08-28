a1088e4b34d6f95644daf73e4c8e70e4bb92cf4d
Aryo Jamousi <aryo.praktikum@codecookies.com>
2020-08-28 12:22:34 +0200

    filewatcher for mac

--- filewatcher.sh
+++ filewatcher.sh
@@ -1,9 +1,26 @@
-if ! command -v inotifywait &> /dev/null
-then
-    echo "Command 'inotifywait' could not be found. Please install 'inotify-tools'"
-    exit
+source ~/.giterminal/helpers/colors.sh
+
+if bash --version | grep -q darwin 
+then 
+	# MacOS
+	if ! command -v fswatch &> /dev/null
+	then
+			echo "$(cyan fswatch) could not be found. Please install 'fswatch'"
+			exit
+	fi
+	fswatch -xrL ./* |
+		while read file event; do
+			./installer.sh
+		done
+else
+	# Linux
+	if ! command -v inotifywait &> /dev/null
+	then
+			echo "$(cyan inotifywait) could not be found. Please install 'inotify-tools'"
+			exit
+	fi
+	inotifywait -m ./* -e close_write |
+		while read dir action file; do
+			./installer.sh
+		done
 fi
-inotifywait -m ./* -e close_write |
-	while read dir action file; do
-		./installer.sh
-	done
