23ef17c594d1040349e464a6c8f024a2fc02a799
aryo <aryo@pavilion.fritz.box>
2022-08-22 14:19:03 +0200

    installer for python + readme

---
+++ python/README.md
@@ -0,0 +1,8 @@
+## Installation
+1. clone giterminal
+`git clone https://github.com/Aryojaam/giterminal.git`
+
+2. run the installer by
+`./installer`
+
+you can now use giterminal by running `giterminal` in your terminal :)
---
+++ python/installer.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+mainDirectory=".giterminal"
+
+rcFile=~/".bashrc"
+if bash --version | grep -q darwin
+then 
+   rcFile=~/".bash_profile"
+fi
+
+mainAlias="alias giterminal='$(pwd)/term.py'"
+# updaterAlias="alias giterminalUpdater='~/$mainDirectory/updater'"
+aliasFound=$(cat $rcFile | grep giterminal=)
+if [ "$aliasFound" == "$mainAlias" ]; then
+    # lineNumber=$(grep -n giterminal= ${rcFile} | cut -d : -f 1)
+    # sed -i `${lineNumber}s/.*/"${mainAlias}"/` $rcFile
+    echo "Alias already there"
+else
+    echo $mainAlias >> $rcFile
+    echo "giterminal installed successfully"
+    exec bash
+fi
